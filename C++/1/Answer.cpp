ca839c2c017cf971a8a62f2ca14afc017cbd2a8e
unknown <hassanabdulgader16@gmail.com>
2023-09-22 12:07:17 -0700

    This is for problems solving

---
+++ C++/1/Answer.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Employee {
+	public:
+		int id;
+		string name;
+		double age;
+};
+int main(){
+	Employee e;
+
+	cout<<"Please Enter Your ID: ";
+	cin>>e.id;
+	
+	cout<<"Please Enter Your Name: ";
+	cin>>e.name;
+	
+	cout<<"Please Enter Your Age: ";
+	cin>>e.age;
+
+	cout<<e.id<<"  -  "<<e.name<<"  -  "<<e.age<<endl;
+	
+}
